96bd0ac1de86f9607c0d7a31e41dad321cd06761
esbenpalmstrom <esbenpalmstroem@gmail.com>
2021-11-26 15:38:57 +0100

    initial commit

---
+++ runfullstack.jl
@@ -0,0 +1,11 @@
+import Dates
+stack_t_start = Dates.now()
+
+include("init_basin.jl")
+include("compact_basin.jl")
+include("layer_basin.jl")
+include("deform_basin.jl")
+
+stack_t_now = Dates.now()
+stack_dur = Dates.canonicalize(stack_t_now-stack_t_start)
+print("Time elapsed: ",stack_dur)
