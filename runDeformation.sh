cb5c9f43f31997c8d17ec57187a49788687a1715
esbenpalmstrom <esbenpalmstroem@gmail.com>
2022-02-15 16:08:15 +0100

    Not minor changes

--- runDeformation.sh
+++ runDeformation.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-julia deform_basin.jl 1 1000 0.12 10.0 true "fixed" 0.10 0.5 0.0 0.5 3.5e7 0.5e7 0.100 0.450 0.5 0.01 0.5 0.01 0.5 0.05 1 2 5.0 934.0 934.0 "simple" false
+julia deform_basin.jl 16 40000 0.12 10.0 false "fixed" 0.0 0.5 0.0 0.5 3.5e7 0.5e7 0.185 0.300 0.7 0.01 0.7 0.01 0.25 0.05 1 2 5.0 934.0 934.0 "simple" false
--- shear_visualization.jl
+++ shear_visualization.jl
@@ -1,30 +1,46 @@
 include("Granular/src/Granular.jl")
 import JLD2
+import Statistics
 
-
-
-id_nr = 1 # id number of simulation
-grain_nr = 1000 # number of grains in simulation
+id_nr = 2 # id number of simulation
+grain_nr = 500 # number of grains in simulation
 
 sim = Granular.readSimulation("simulation$(grain_nr)/deformed$(id_nr).jld2")
 
 SimSettings = JLD2.load("simulation$(grain_nr)/SimSettings.jld2")
 
 r_max = SimSettings["r_max"]
+r_min = SimSettings["r_min"]
+r_av = (r_max+r_min)/2
 
 
 
+global shear_strain_array = zeros(2,size(sim.grains,1))
+
+global i = 1
+
+global j = 0
+
+global k = 1
 
 for grain_i in sim.grains
     shear_strain = zeros(2)
 
-    for grain_j in sim.grains
-        dx = grain_j.lin_po[1] - grain_i.lin_pos[1]
-        dy = grain_j.lin_po[2] - grain_i.lin_pos[2]
+    global k = 1
 
-        if (dx > r_max*2 || dy > r_max*2 || grain_i==grain_j):
+    for grain_j in sim.grains
+        dx = grain_j.lin_pos[1] - grain_i.lin_pos[1]
+        dy = grain_j.lin_pos[2] - grain_i.lin_pos[2]
+        dist = sqrt((dx^2)+(dy^2))
+
+        #if (dx > (r_av*2) || dy > (r_av*2) || grain_i==grain_j)
+        if (dist > r_av*3 || grain_i == grain_j || grain_j.color == -1 || grain_j.color == 0 || dist < (r_av/2))
             continue
         end
+
+        @info "accepted dist: $(dist), this is number $(j), and it is number $(k) contact for this grain"
+        global j += 1
+        global k += 1
 
         disp_x = grain_j.lin_disp[1] - grain_i.lin_disp[1]
         disp_y = grain_j.lin_disp[2] - grain_i.lin_disp[2]
@@ -32,9 +48,32 @@ for grain_i in sim.grains
         shear_strain[1] += disp_y/dx
         shear_strain[2] += disp_x/dy
 
+        if (shear_strain[1] > 100 || shear_strain[2] > 100)
+            @warn "shear strain of over 100 was detected for grain $(i), with disp_x of $(disp_x) and disp_y of $(disp_y)
+            and dx of $(dx) and dy of $(dy)"
+        end
+
     end
-    grain_i.shear_strain = shear_strain
+
+    shear_strain_array[:,i] = shear_strain
+
+    if (grain_i.color == 0 || grain_i.color == -1)
+        shear_strain_array[:,i] = [0.0,0.0]
+    end
+
+
+
+
+    global i+=1
+
 end
 
+maxstrain = maximum(shear_strain_array)
+
+@info "max shear strain: $(maxstrain)
+        average: $(Statistics.mean(shear_strain_array))"
+
+
 Granular.writeVTK(sim,
-                  folder = "simulation$(grain_nr)/deformed$(id_nr)_shear_strain")
+                  folder = "simulation$(grain_nr)/deformed$(id_nr)_shear_strain",
+                  shear_strain_matrix=shear_strain_array)
